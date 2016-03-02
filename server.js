60fc4f76a78d1637838ffa42dfe0d3a4a3c9d4bc
Kevin Lowe <iamkevinlowe@gmail.com>
2016-03-02 10:51:30 -0800

    Added privacy policy.  Added support for instagram's oauth procedure.

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var request = require('request');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
 
 const VERIFY_TOKEN = Math.random().toString(36).substring(2);
 
@@ -10,10 +12,25 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/styles'));
 app.use(express.static(__dirname + '/scripts'));
 
-app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'));
+app.use(cookieParser());
+app.use(session({secret: process.env.SECRET}));
+
+app.get('/', function(req, res) {
+  if (req.session.access_token) {
+    res.sendFile(__dirname + '/views/index.html');
+  } else {
+    res.redirect('https://api.instagram.com/oauth/authorize?client_id=' + process.env.INSTAGRAM_CLIENT_ID + '&redirect_uri=http://localhost:5000/auth&response_type=code');
+  }
+});
+
+app.get('/auth', function(req, res) {
+  req.session.access_token = req.query.code;
+  res.redirect('/');
+});
 
 app.get('/images', function(req, res) {
   var url = process.env.INSTAGRAM_URL + '/tags/' + req.query.tag + '/media/recent?client_id=' + process.env.INSTAGRAM_CLIENT_ID;
+  // var url = process.env.INSTAGRAM_URL + '/tags/' + req.query.tag + '/media/recent?access_token=' + req.session.access_token;
   if (req.query.minTagId) url += '&min_tag_id=' + req.query.minTagId;
   if (req.query.maxTagId) url += '&max_tag_id=' + req.query.maxTagId;
   request.get(url, (error, response, body) => error ? console.error('Error: ', error) : res.send(body));
